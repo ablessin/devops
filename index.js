fdb0a49c3fc2a9efb3d66837c41cdbb9642dd2ba
Blessing Arnaud <blessarnaud@gmail.com>
2022-05-18 09:46:45 +0200

    test

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
 /** Import env variables */
-require('dotenv').config({ 
-    path: process.env.NODE_ENV === "test" ? `.env.test` : undefined 
-})
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? `.env.test` : undefined,
+});
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 /** Setup app */
-app.use(cors())
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
+// TEST
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
-    res.send(`Hello app, running on: ${process.env.NODE_ENV}`)
-})
+  res.send(`Hello app, running on: ${process.env.NODE_ENV}`);
+});
 
 if (process.env.NODE_ENV !== "test")
-    app.listen(port, () =>
-        console.log(`Example app listening at http://localhost:${port}`)
-    )
+  app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+  );
 
-module.exports = app
+module.exports = app;
